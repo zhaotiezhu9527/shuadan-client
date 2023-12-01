5a59743c1b526f7d493cd78457e707e401843bc0
water <watershui122@gmail.com>
2023-12-01 17:56:33 +0700

    客服

--- shuadan-Japan/plugins/base.js
+++ shuadan-Japan/plugins/base.js
@@ -31,8 +31,8 @@ const PATH_URL =
   process.env.NODE_ENV === "development"
     //  ? "http://8.222.224.10:9544/" //本地环境
     ? "http://sdqt.juhai.xyz/sd-qtapi-test/" //测试环境
-    // :  "/sd-qtapi-test/"; //测试环境
-    :  "/api/"; //越南刷单正式接口
+    :  "/sd-qtapi-test/"; //测试环境
+    // :  "/api/"; //越南刷单正式接口
     // :  "https://sdpp1100.top/sdpp1/"; //越南app
     
 
