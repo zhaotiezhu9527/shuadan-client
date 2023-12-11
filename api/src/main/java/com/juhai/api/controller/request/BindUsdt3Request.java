58399b068ef4d2aad9d02095cf19d341581eed83
zhaotiezhu <zhaot0392@gmail.com>
2023-12-11 18:38:07 +0700

    删除身份证图片字段

--- api/src/main/java/com/juhai/api/controller/request/BindUsdt3Request.java
+++ api/src/main/java/com/juhai/api/controller/request/BindUsdt3Request.java
@@ -25,8 +25,4 @@ public class BindUsdt3Request {
     @NotNull(message = "system.param.err")
     @ApiModelProperty(value = "身份号码", example = "身份号码", required = true)
     private String idCard;
-
-    @NotNull(message = "system.param.err")
-    @ApiModelProperty(value = "身份证图片", example = "身份证图片", required = true)
-    private String idCardImg;
 }
